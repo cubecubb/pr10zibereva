773ba1ec96cf9ef3f926665573cd152138c31bad
cubecubb <annazibereva999@gmail.com>
2022-06-10 11:32:42 +0700

    PROJECT

---
+++ PR10zibereva/app/src/main/java/com/example/pr10zibereva/activity_2.kt
@@ -0,0 +1,11 @@
+package com.example.pr10zibereva
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
